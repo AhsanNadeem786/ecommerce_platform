790a2dfbab7cf898cf7aa2413229889e604e5787
ahsannadeemofficial215 <ahsannadeemofficial215@gmail.com>
2026-07-14 16:30:04 +0500

    feat: add JWT authentication to proxy for user validation and redirection

--- proxy.ts
+++ proxy.ts
@@ -1,14 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
+import jwt from 'jsonwebtoken';
+import User from "./models/User";
 
-export default function Proxy(request:NextRequest) {
-    const token = request.cookies.get('token')?.value
+export default async function Proxy(request: NextRequest) {
+    const token = request.cookies.get('token')?.value;
+
     if (request.nextUrl.pathname.startsWith('/e-commerce') && !token) {
- 
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    if (request.nextUrl.pathname.startsWith("/invantory")) {
+        if (!token) {
+            return NextResponse.redirect(new URL('/login', request.url));
+        }
+
+        try {
+            const decoded = jwt.verify(token, 'screct-key') as { userId: string };
+            const userId = decoded.userId;
+
+            const user = await User.findOne({ _id: userId, userStatus: 'active' });
+
+            if (!user) {
+                return NextResponse.redirect(new URL('/e-commerce', request.url));
+            }
+        } catch (error) {
+            return NextResponse.redirect(new URL('/profile', request.url));
+        }
+    }
+
+    return NextResponse.next();
 }
+
 export const config = {
-    Matcher:[
-        '/e-commerce'
+    matcher: [
+        '/e-commerce',
+        '/invantory/:path*'
     ]
-}
+};
