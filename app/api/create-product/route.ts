64fb1be402d2e85ec361faa932eab8e9b2ac58dc
ahsannadeemofficial215 <ahsannadeemofficial215@gmail.com>
2026-07-20 11:39:48 +0500

    Debug create-product API

--- app/api/create-product/route.ts
+++ app/api/create-product/route.ts
@@ -2,14 +2,14 @@ import dbConnect from "@/lib/dbConnect";
 import product from "@/models/createproduct";
 import { cookies } from "next/headers";
 import jwt from 'jsonwebtoken';
-import Cart from "@/models/cart";
+
 export async function POST(request: Request) {
     await dbConnect();
     try {
         const body = await request.json();
 
         const productData = await product.create({
-            images: body.images,
+            images:body.images,
             name: body.productName,
             price: body.price,
             quantity: body.quantity,
@@ -22,38 +22,41 @@ export async function POST(request: Request) {
         return Response.json({ message: "Product created successfully", data: productData }, { status: 201 });
     } catch (error) {
         console.log(error);
-
+        
         return Response.json({ error: "Failed to create product" }, { status: 500 });
 
     }
-
+    
 }
-
-export async function GET() {
+ 
+ export async function GET() {
     await dbConnect();
     try {
         const cokkieStore = await cookies()
-        const token = cokkieStore.get("token")?.value
-
-        if (!token) throw new Error("No token found");
-
-
-        const decoded = jwt.verify(token, 'screct-key') as { userId: string }
-        const userId = decoded.userId;
-        const productData = await product.find().populate("categoryId")
-
-
-// .populate({
-//             path: "isCart",
-//             match: { UserId: userId }
-//         })
+          const token = cokkieStore.get("token")?.value
+        
+          if (!token) throw new Error("No token found");
+        
+        
+          const decoded = jwt.verify(token, 'screct-key')as{userId:string}
+          const userId = decoded.userId;
+        const productData = await product.find().populate("categoryId").populate({path:"isCart",
+            match:{UserId:userId}
+        })
+ 
+        
+        
         return Response.json({ message: "Products fetched successfully", data: productData }, { status: 200 });
     } catch (error: any) {
-        console.error(error);
+    console.error("GET /api/create-product Error:", error);
 
-        return Response.json({
-            message: error.message,
-            stack: error.stack,
-        }, { status: 500 });
-    }
+    return Response.json(
+      {
+        success: false,
+        message: error.message,
+        stack: process.env.NODE_ENV === "development" ? error.stack : undefined,
+      },
+      { status: 500 }
+    );
+}
 }
