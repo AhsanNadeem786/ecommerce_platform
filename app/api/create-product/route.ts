5ce0dad65a255bafd20e885d3a08d2670dba02dd
ahsannadeemofficial215 <ahsannadeemofficial215@gmail.com>
2026-07-20 11:43:18 +0500

    Debug create-product API

--- app/api/create-product/route.ts
+++ app/api/create-product/route.ts
@@ -40,11 +40,11 @@ export async function POST(request: Request) {
         
           const decoded = jwt.verify(token, 'screct-key')as{userId:string}
           const userId = decoded.userId;
-        const productData = await product.find().populate("categoryId").populate({path:"isCart",
-            match:{UserId:userId}
-        })
+        const productData = await product.find().populate("categoryId")
  
-        
+        // .populate({path:"isCart",
+        //     match:{UserId:userId}
+        // })
         
         return Response.json({ message: "Products fetched successfully", data: productData }, { status: 200 });
     } catch (error: any) {
