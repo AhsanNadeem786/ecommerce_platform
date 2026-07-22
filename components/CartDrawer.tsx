1cfffb2d84556a653bd6dfbdadb6db28275e1735
ahsannadeemofficial215 <ahsannadeemofficial215@gmail.com>
2026-07-22 15:57:00 +0500

    Fix CartDrawer item state initialization and update badge condition for cart count display

--- components/CartDrawer.tsx
+++ components/CartDrawer.tsx
@@ -14,7 +14,7 @@ const CartDrawer = () => {
     const [deleteAllCart, setDeleteAllCart] = useState();
     const router = useRouter()
     const pathname = usePathname()
-    const [item, setItem] = useState([])
+   const [item, setItem] = useState<number>(0);
 
 
     const fetchCart = async () => {
@@ -109,12 +109,11 @@ const CartDrawer = () => {
     return (
 
         <Drawer open={open} onOpenChange={cartopenChange} direction="right">
-
             <DrawerTrigger asChild>
                 <Button className="cursor-pointer bg-white hover:bg-gray-300">
-                    {item.length > 0 && (
+                    {item !== 0 && (
                         <span className="bg-red-500 text-[9px] font-bold text-white p-0.5 w-4 h-4 rounded-full">
-                            {item.length}
+                            {item}
                         </span>
                     )}
 
